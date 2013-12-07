ebd7b6c61fefa0f4a3457ec390ed48cbfbf1e954
Zac Fowler <zfowler@unomaha.edu>
2013-12-06 23:40:01 -0600

    Working commit

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\gen\io\cordova\hellocordova\R.java \
+ : C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\drawable\icon.png \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\values\strings.xml \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\res\xml\config.xml \
+C:\Users\Zac\Documents\GitHub\my-c-app\platforms\android\bin\AndroidManifest.xml \
